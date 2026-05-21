c8631e53b5c3b558b4aff2ca4f8f66e8d3fbba06
KarenOs25 <karen.ospina73@correo.tdea.edu.co>
2026-05-21 03:08:36 +0000

    correcciones1.4

--- js/auth.js
+++ js/auth.js
@@ -1,29 +1,18 @@
 const Auth = {
-    registrar() {
-        const user = document.querySelector('input[type=text]').value;
-        const pass = document.querySelector('input[type=password]').value;
-        if(!user || !pass) return alert("Completa los campos");
-        
-        const users = JSON.parse(localStorage.getItem('users') || '[]');
-        users.push({ user, pass });
-        localStorage.setItem('users', JSON.stringify(users));
-        alert("Registro exitoso");
-        window.location.href = 'login.html';
-    },
-
     login() {
-        const user = document.querySelector('input[type=text]').value;
-        const pass = document.querySelector('input[type=password]').value;
+        const u = document.getElementById('user').value;
+        const p = document.getElementById('pass').value;
         const users = JSON.parse(localStorage.getItem('users') || '[]');
         
-        if (users.find(u => u.user === user && u.pass === pass)) {
+        const validUser = users.find(user => user.user === u && user.pass === p);
+        
+        if (validUser) {
             sessionStorage.setItem('session', 'true');
             window.location.href = 'index.html';
         } else {
-            alert("Credenciales incorrectas");
+            alert("Usuario o contraseña incorrectos");
         }
     },
-
     logout() {
         sessionStorage.removeItem('session');
         window.location.href = 'login.html';
