d5873c4b0c0a94a580843cb37ba33da79fc5a19b
KarenOs25 <karen.ospina73@correo.tdea.edu.co>
2026-05-21 03:48:46 +0000

    correcciones1.8

--- js/auth.js
+++ js/auth.js
@@ -1,12 +1,31 @@
 const Auth = {
+    registrar() {
+        const u = document.getElementById('u').value;
+        const p = document.getElementById('p').value;
+        if (!u || !p) return alert("Completa los campos");
+        
+        let users = JSON.parse(localStorage.getItem('users') || '[]');
+        if (users.find(x => x.user === u)) return alert("Usuario ya existe");
+        
+        users.push({ user: u, pass: p });
+        localStorage.setItem('users', JSON.stringify(users));
+        alert("Usuario creado. Ahora ingresa.");
+        window.location.href = 'login.html';
+    },
+
     login() {
         const u = document.getElementById('u').value;
         const p = document.getElementById('p').value;
-        if (u === "admin" && p === "123") {
+        const users = JSON.parse(localStorage.getItem('users') || '[]');
+        
+        if (users.find(x => x.user === u && x.pass === p)) {
             sessionStorage.setItem('session', 'true');
             window.location.href = 'index.html';
-        } else { alert("Usuario o contraseña incorrectos"); }
+        } else {
+            alert("Usuario o contraseña incorrectos");
+        }
     },
+
     logout() {
         sessionStorage.removeItem('session');
         window.location.href = 'login.html';
